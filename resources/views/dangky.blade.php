77e160fee86aac7a05b7e6efd4b7b0831973b263
tham99 <nguyenthithammwg1@gmail.com>
2020-08-26 08:54:15 +0700

    .

---
+++ resources/views/dangky.blade.php
@@ -0,0 +1,30 @@
+<div class="container">
+  <form action="">
+    <div class="row">
+      <div class="col-10">
+        <label for="fname">LIÊN HỆ</label>
+      </div>
+      <div class="col-75">
+        <input type="text" id="fname" name="firstname" placeholder="Your name..">
+      </div>
+    </div>
+
+    <div class="row">
+        <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+    </div>
+
+    <div class="row">
+        <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+    </div>
+    
+    <div class="row">
+        <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+    </div>
+     <hr  width="30%" align="left" />
+     <hr width="2" size="400" align="center">
+
+      <div class="col-25">
+        <label for="fname"></label>
+      </div>
+  </form>
+</div>
---
+++ resources/views/dangkyntd.blade.php
@@ -0,0 +1,140 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Đăng ký nhà tuyển dụng</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="manifest" href="site.webmanifest">
+    <link rel="shortcut icon" type="image/x-icon" href="admin/img/favicon.ico">
+
+	<!-- CSS here -->
+	<link rel="stylesheet" href="admin/css/bootstrap.min.css">
+	<link rel="stylesheet" href="admin/css/owl.carousel.min.css">
+	<link rel="stylesheet" href="admin/css/slicknav.css">
+    <link rel="stylesheet" href="admin/css/flaticon.css">
+    <link rel="stylesheet" href="admin/css/progressbar_barfiller.css">
+    <link rel="stylesheet" href="admin/css/gijgo.css">
+    <link rel="stylesheet" href="admin/css/animate.min.css">
+    <link rel="stylesheet" href="admin/css/animated-headline.css">
+	<link rel="stylesheet" href="admin/css/magnific-popup.css">
+	<link rel="stylesheet" href="admin/css/fontawesome-all.min.css">
+	<link rel="stylesheet" href="admin/css/themify-icons.css">
+	<link rel="stylesheet" href="admin/css/slick.css">
+	<link rel="stylesheet" href="admin/css/nice-select.css">
+	<link rel="stylesheet" href="admin/css/style.css">
+</head>
+<body>
+    <!-- ? Preloader Start -->
+    <div id="preloader-active">
+        <div class="preloader d-flex align-items-center justify-content-center">
+            <div class="preloader-inner position-relative">
+                <div class="preloader-circle"></div>
+                <div class="preloader-img pere-text">
+                    <img src="admin/img/logo/loder.png" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Preloader Start-->
+
+
+<!-- Register -->
+
+<main class="login-body" data-vide-bg="admin/img/login-bg.mp4">
+    <!-- Login Admin -->
+    <form class="form-default" action="login-bg.mp4" method="POST">
+        
+        <div class="login-form">
+            <!-- logo-login -->
+            <div class="logo-login">
+                <a href="index.html"><img src="admin/img/logo/loder.png" alt=""></a>
+            </div>
+            <h2>Đăng ký ứng viên</h2>
+
+            <div class="form-input">
+                <label for="name">Họ Tên</label>
+                <input  type="text" name="name" placeholder="Nhập họ tên">
+            </div>
+            <div class="form-input">
+                <label for="name">Số điện thoại</label>
+                <input  type="text" name="name" placeholder="Nhập số điện thoại">
+            </div>
+            <div class="form-input">
+                <label for="name">Tên công ty</label>
+                <input  type="text" name="name" placeholder="Nhập họ tên">
+            </div>
+            <div class="form-input">
+                <label for="name">Địa chỉ</label>
+                <input  type="text" name="name" placeholder="Nhập họ tên">
+            </div>
+            <div class="form-input">
+                <label for="name">Email</label>
+                <input type="email" name="email" placeholder="Nhập email">
+            </div>
+            <div class="form-input">
+                <label for="name">Mật khẩu</label>
+                <input type="password" name="password" placeholder="Nhập mật khẩu">
+            </div>
+            <div class="form-input">
+                <label for="name">Xác nhận mật khẩu</label>
+                <input type="password" name="password" placeholder="Nhập lại mật khẩu">
+            </div>
+            <div class="form-input pt-30">
+                <input type="submit" name="submit" value="Registration">
+            </div>
+            <!-- Forget Password -->
+            <!-- <a href="login.html" class="registration">login</a> -->
+        </div>
+    </form>
+    <!-- /end login form -->
+</main>
+
+
+    <script src="admin/js/vendor/modernizr-3.5.0.min.js"></script>
+    <!-- Jquery, Popper, Bootstrap -->
+    <script src="admin/js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="admin/js/popper.min.js"></script>
+    <script src="admin/js/bootstrap.min.js"></script>
+    <!-- Jquery Mobile Menu -->
+    <script src="admin/js/jquery.slicknav.min.js"></script>
+
+    <!-- Video bg -->
+    <script src="admin/js/jquery.vide.js"></script>
+
+    <!-- Jquery Slick , Owl-Carousel Plugins -->
+    <script src="admin/js/owl.carousel.min.js"></script>
+    <script src="admin/js/slick.min.js"></script>
+    <!-- One Page, Animated-HeadLin -->
+    <script src="admin/js/wow.min.js"></script>
+    <script src="admin/js/animated.headline.js"></script>
+    <script src="admin/js/jquery.magnific-popup.js"></script>
+
+    <!-- Date Picker -->
+    <script src="admin/js/gijgo.min.js"></script>
+    <!-- Nice-select, sticky -->
+    <script src="admin/js/jquery.nice-select.min.js"></script>
+    <script src="admin/js/jquery.sticky.js"></script>
+    <!-- Progress -->
+    <script src="admin/js/jquery.barfiller.js"></script>
+    
+    <!-- counter , waypoint,Hover Direction -->
+    <script src="admin/js/jquery.counterup.min.js"></script>
+    <script src="admin/js/waypoints.min.js"></script>
+    <script src="admin/js/jquery.countdown.min.js"></script>
+    <script src="admin/js/hover-direction-snake.min.js"></script>
+
+    <!-- contact js -->
+    <script src="admin/js/contact.js"></script>
+    <script src="admin/js/jquery.form.js"></script>
+    <script src="admin/js/jquery.validate.min.js"></script>
+    <script src="admin/js/mail-script.js"></script>
+    <script src="admin/js/jquery.ajaxchimp.min.js"></script>
+    
+    <!-- Jquery Plugins, main Jquery -->	
+    <script src="admin/js/plugins.js"></script>
+    <script src="admin/js/main.js"></script>
+    
+    </body>
+</html>
